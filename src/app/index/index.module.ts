896cd48979ceedb3c2131d3672f316a0e61f096f
unknown <karanbothra12@gmail.com>
2019-05-26 21:10:20 +0530

    Privacy and FAQ page added

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -12,12 +12,14 @@ import { IndexComponent } from './index.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PrivacyPolicyComponent } from './privacypolicy/privacypolicy.component';
+import { FAQComponent } from './faq/faq.component';
 import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
 	imports: [ CommonModule, ProductModule, SharedModule, RouterModule.forChild(IndexRoutes) ],
-	declarations: [ IndexComponent, NavbarComponent, LoginComponent,FooterComponent ],
+	declarations: [ IndexComponent, NavbarComponent, LoginComponent,FooterComponent,PrivacyPolicyComponent, FAQComponent],
 	schemas: [ NO_ERRORS_SCHEMA ],
 	exports: [ NavbarComponent,FooterComponent ],
 	providers: []
--- src/app/index/index.routing.ts
+++ src/app/index/index.routing.ts
@@ -2,7 +2,8 @@ import { LoginComponent } from "./login/login.component";
 import { Routes } from "@angular/router";
 import { IndexComponent } from "./index.component";
 import { AuthGuard } from '../shared/services/auth_gaurd';
-
+import { PrivacyPolicyComponent } from './privacypolicy/privacypolicy.component';
+import { FAQComponent } from './faq/faq.component';
 export const IndexRoutes: Routes = [
   {
     path: "",
@@ -15,6 +16,14 @@ export const IndexRoutes: Routes = [
       {
         path: "login",
         component: LoginComponent
+      },
+      {
+        path: "privacypolicy",
+        component: PrivacyPolicyComponent
+      },
+      {
+        path: "faq",
+        component: FAQComponent
       }
     ]
   }
